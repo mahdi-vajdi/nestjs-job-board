001e8352308121a1cbe092227b59a4cf7ebcebbc
mahdi-vajdi <mahdivajdii@gmail.com>
2025-08-09 22:39:45 +0330

    feat(common): add pagination helpers

---
+++ src/common/pagination/paginated-result.ts
@@ -0,0 +1,16 @@
+import { IPaginatedResult } from '@common/pagination/pagination.model';
+import { ILimitationOptions } from '@common/pagination/limitation.interface';
+
+export class PaginatedResult<T> implements IPaginatedResult<T> {
+  constructor(queryResult: T[], total: number, limitation: ILimitationOptions) {
+    this.list = queryResult;
+    this.total = total;
+    this.page = limitation.skip / limitation.limit + 1;
+    this.pageSize = limitation.limit;
+  }
+
+  list: T[];
+  page: number;
+  pageSize: number;
+  total: number;
+}
